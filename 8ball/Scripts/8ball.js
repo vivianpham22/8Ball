9f0f2c663c46e4b43ace2cc06e50ccbf0066d4e7
kingveee <vivianpham22@gmail.com>
2017-08-08 14:43:23 -0400

    first commit

---
+++ 8ball/Scripts/8ball.js
@@ -0,0 +1,8 @@
+﻿function getPhrase() {
+    var phrases = ['Maybe', 'Absolutely', 'Not A Chance', 'Outlook Good', 'Try Again', 'It Is Certain', 'Better Not Tell You Now', 'Dont Count on It', 'Very doubtful'];
+
+    var num = Math.floor(Math.random() * phrases.length);
+    document.getElementById("fortune").innerHTML = phrases[num];
+    
+}
+
